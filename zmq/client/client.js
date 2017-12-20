3d5d21ca428a1f80810fa19f6d8d1307e3997633
navono <navono007@gmail.com>
2017-12-20 14:48:27 +0800

    delete from toot

--- zmq/client/client.js
+++
@@ -1,7 +0,0 @@
-// Create PULL socket
-const socket = require('zmq').socket(`pull`);
-
-socket.connect(`tcp://127.0.0.1:3000`);
-socket.on('message', msg => {
-  console.log(`Message received: ${msg}`);
-});
